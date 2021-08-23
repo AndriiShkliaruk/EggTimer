b0b0fa7e777c93a4c9fc95a0b3d7675ecc53c736
Andrii Shkliaruk <devshkliaruk@gmail.com>
2021-08-23 20:08:48 +0300

    Timer function created

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var timerCount = 0
+        
+        switch segue.identifier {
+        case "SoftBoiledTimer":
+            timerCount = 4
+        case "MediumBoiledTimer":
+            timerCount = 6
+        case "HardBoiledTimer":
+            timerCount = 10
+        default:
+            return
+        }
+        
+        guard let destination = segue.destination as? TimerViewController else { return }
+        destination.timerCount = timerCount
     }
 
 
